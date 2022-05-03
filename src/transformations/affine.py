8923b4398fd1fb3225deb1ba57365542953acb4b
JamesAllingham <james.allingham@gmail.com>
2022-05-03 12:56:43 +0100

    Formatting and other small changes

--- src/transformations/affine.py
+++ src/transformations/affine.py
@@ -5,6 +5,28 @@ from jax.scipy.linalg import expm
 from chex import Array, assert_shape, assert_rank, assert_equal_shape
 
 
+def create_generator_matrices() -> Array:
+    """Creates the generator matrices for affine transformations.
+    See https://en.wikipedia.org/wiki/Affine_transformation#Image_transformation.
+
+    Args:
+        None
+
+    Returns:
+        A 6x3x3 array containing the 6 3x3 generator matrices, in order:
+        translation in x, translation in y, rotation, scale in x, scale in y, and shearing.
+    """
+    G_trans_x = jnp.zeros((3, 3), jnp.float32).at[0, 2].set(1)
+    G_trans_y = jnp.zeros((3, 3), jnp.float32).at[1, 2].set(1)
+    G_rot = jnp.zeros((3, 3), jnp.float32).at[1, 0].set(1).at[0, 1].set(-1)
+    G_scale_x = jnp.zeros((3, 3), jnp.float32).at[0, 0].set(1)
+    G_scale_y = jnp.zeros((3, 3), jnp.float32).at[1, 1].set(1)
+    G_shear = jnp.zeros((3, 3), jnp.float32).at[1, 0].set(1).at[0, 1].set(1)
+    Gs = jnp.array([G_trans_x, G_trans_y, G_rot, G_scale_x, G_scale_y, G_shear])
+
+    return Gs
+
+
 def gen_transform_mat(
     η: Array,
     ε: Array,
@@ -33,13 +55,7 @@ def gen_transform_mat(
     assert_equal_shape([η, ε])
     assert_shape(η, (6,))
 
-    G_trans_x = jnp.zeros((3, 3), jnp.float32).at[0, 2].set(1)
-    G_trans_y = jnp.zeros((3, 3), jnp.float32).at[1, 2].set(1)
-    G_rot = jnp.zeros((3, 3), jnp.float32).at[1, 0].set(1).at[0, 1].set(-1)
-    G_scale_x = jnp.zeros((3, 3), jnp.float32).at[0, 0].set(1)
-    G_scale_y = jnp.zeros((3, 3), jnp.float32).at[1, 1].set(1)
-    G_shear = jnp.zeros((3, 3), jnp.float32).at[1, 0].set(1).at[0, 1].set(1)
-    Gs = jnp.array([G_trans_x, G_trans_y, G_rot, G_scale_x, G_scale_y, G_shear])
+    Gs = create_generator_matrices()
 
     T = expm((ε[:, jnp.newaxis, jnp.newaxis] * η[:, jnp.newaxis, jnp.newaxis] * Gs).sum(axis=0))
 
@@ -50,12 +66,12 @@ def transform_image(
     image: Array,
     T: Array
 ) -> Array:
-    """Applies and affine transformation to and image.
+    """Applies an affine transformation to an image.
 
     See Sec 3.2 in "Spatial Transformer Networks" by Jaderberg et al.
 
     Args:
-        image: a rank-3 Array of shape (num channels, heigh, width).
+        image: a rank-3 Array of shape (num channels, height, width).
 
         T: a 3x3 affine transformation Array.
 
