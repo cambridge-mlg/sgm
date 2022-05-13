eef3e2d4ed140b183c0877965689e8f0fa8fdb99
JamesAllingham <james.allingham@gmail.com>
2022-05-13 09:46:02 +0100

    Made a separate utils.jax which contains tree_transpose

--- src/utils/training.py
+++ src/utils/training.py
@@ -18,6 +18,7 @@ from ml_collections import config_dict
 from clu import parameter_overview
 
 from src.utils.plotting import plot_img_array
+from src.utils.jax import tree_transpose
 from src.data import NumpyLoader
 import src.models as models
 
@@ -269,7 +270,7 @@ def train_loop(
             rng, test_rng = random.split(rng)
             if val_losses[-1] <= min(val_losses):
                 print("Best val_loss")
-                # TODO add model saving.
+                # TODO: add model saving.
 
                 run.summary['best_epoch'] = epoch
                 run.summary['best_val_loss'] = val_losses[-1]
@@ -295,8 +296,3 @@ def train_loop(
                         sampled_images, title=samples_plot_title)
 
     return state
-
-
-def tree_transpose(list_of_trees):
-    """Convert a list of trees of identical structure into a single tree of lists."""
-    return jax.tree_map(lambda *xs: jnp.array(list(xs)), *list_of_trees)
