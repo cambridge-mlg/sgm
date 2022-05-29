0909bef26392432a81e2f64aea77afa28f4ef616
JamesAllingham <james.allingham@gmail.com>
2022-05-29 17:33:59 +0100

    Update default configs to a faster training version

--- experiments/inv_vae_mnist_default.py
+++ experiments/inv_vae_mnist_default.py
@@ -11,7 +11,7 @@ def get_config() -> config_dict.ConfigDict:
     config.dataset_name = 'MNIST'
     config.val_percent = 0.1
     config.batch_size = 256
-    config.epochs = 50
+    config.epochs = 25
 
     config.optim_name = 'adamw'
     config.optim = config_dict.ConfigDict()
@@ -29,30 +29,29 @@ def get_config() -> config_dict.ConfigDict:
 
     config.model_name = 'invVAE'
     config.model = config_dict.ConfigDict()
-    config.model.latent_dim = 32
+    config.model.latent_dim = 128
     config.model.learn_prior = False
-    config.model.architecture = 'ConvNeXt'  # 'ConvNet'  # 'MLP'
+    config.model.architecture = 'ConvNet'
     config.model.η_high = [0., 0., jnp.pi/4, 0., 0., 0., 0.]
     config.model.η_low = [0., 0., -jnp.pi/4, 0., 0., 0., 0.]
     config.model.encoder_invariance = 'partial'  # 'none'  # 'full'
     config.model.invariance_samples = 4
 
-    config.β = 10  # 10
+    config.β = 10
     config.β_schedule_name = 'cosine_decay_schedule'
     config.β_schedule = config_dict.ConfigDict()
-    config.β_end_value = 3
+    config.β_end_value = 1
     config.β_schedule.alpha = config.β_end_value / config.β
     config.β_schedule.decay_steps = config.epochs * num_batches_per_epoch
 
     config.model.encoder = config_dict.ConfigDict()
     config.model.encoder.posterior = 'hetero-diag-normal'
-    config.model.encoder.hidden_dims = [64, 128]  # [64, 128, 256]
+    config.model.encoder.hidden_dims = [64, 128, 256]
 
     config.model.decoder = config_dict.ConfigDict()
     config.model.decoder.likelihood = 'iso-normal'
     config.model.decoder.σ_min = 1e-2
     config.model.decoder.hidden_dims = list(reversed(config.model.encoder.hidden_dims))
-    config.model.decoder.hidden_dims = [384, 192, 96, 48]
     config.model.decoder.image_shape = METADATA['image_shape'][config.dataset_name]
 
     return config
