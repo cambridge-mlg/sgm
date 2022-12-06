0fbc7e022caa8c85477ac8a1c5b2e9ab85490a60
JamesAllingham <james.allingham@gmail.com>
2022-12-06 10:58:35 +0000

    Fix plotting for value_range(-1.1) pp.

--- src/utils/plotting.py
+++ src/utils/plotting.py
@@ -4,7 +4,7 @@ from jax import numpy as jnp
 import matplotlib.pyplot as plt
 
 
-def plot_img_array(array, nrow=16, padding=2, pad_value=0.0, title=None):
+def plot_img_array(array, ncol=16, padding=2, pad_value=0.0, title=None):
     array = jnp.asarray(array)
 
     if array.ndim == 4 and array.shape[-1] == 1:  # single-channel images
@@ -12,7 +12,7 @@ def plot_img_array(array, nrow=16, padding=2, pad_value=0.0, title=None):
 
     # Make the mini-batch of images into a grid.
     nmaps = array.shape[0]
-    xmaps = min(nrow, nmaps)
+    xmaps = min(ncol, nmaps)
     ymaps = int(math.ceil(float(nmaps) / xmaps))
     height, width = int(array.shape[1] + padding), int(array.shape[2] + padding)
     num_channels = array.shape[3]
@@ -26,11 +26,12 @@ def plot_img_array(array, nrow=16, padding=2, pad_value=0.0, title=None):
                            x * width + padding:(x + 1) * width].set(array[k])
             k = k + 1
 
-    grid = jnp.clip(grid * 255.0 + 0.5, 0, 255).astype(jnp.uint8)
+    grid = jnp.clip((grid + 1.) * 127.0 + 0.5, 0, 255).astype(jnp.uint8)
 
-    fig = plt.figure(figsize=(1 * xmaps, 1 * ymaps))
+    fig = plt.figure(figsize=(2 * xmaps, 2 * ymaps), dpi=400)
     plt.imshow(grid)
     plt.axis('off')
+    plt.tight_layout()
     if title is not None:
         plt.title(title)
     plt.show()
