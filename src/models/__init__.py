b96a43b1c1cdd946e0d79070236a24be38d231c5
JamesAllingham <james.allingham@gmail.com>
2023-03-23 16:04:34 +0000

    Tidy old models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,18 +4,6 @@ from src.models.livae import (
     make_livae_reconstruction_plot,
     make_livae_sampling_plot,
 )
-from src.models.livae_no_conj1 import (
-    LIVAE_NO_CONJ1,
-    make_livae_no_conj1_batch_loss,
-    make_livae_no_conj1_reconstruction_plot,
-    make_livae_no_conj1_sampling_plot,
-)
-from src.models.livae_no_conj3 import (
-    LIVAE_NO_CONJ3,
-    make_livae_no_conj3_batch_loss,
-    make_livae_no_conj3_reconstruction_plot,
-    make_livae_no_conj3_sampling_plot,
-)
 from src.models.ivae import (
     IVAE,
     make_ivae_batch_loss,
@@ -46,14 +34,6 @@ __all__ = [
     "make_livae_batch_loss",
     "make_livae_reconstruction_plot",
     "make_livae_sampling_plot",
-    "LIVAE_NO_CONJ1",
-    "make_livae_no_conj1_batch_loss",
-    "make_livae_no_conj1_reconstruction_plot",
-    "make_livae_no_conj1_sampling_plot",
-    "LIVAE_NO_CONJ3",
-    "make_livae_no_conj3_batch_loss",
-    "make_livae_no_conj3_reconstruction_plot",
-    "make_livae_no_conj3_sampling_plot",
     "IVAE",
     "make_ivae_batch_loss",
     "make_ivae_reconstruction_plot",
