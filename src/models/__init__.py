b7d24513362707fd343baf077024193834de6966
JamesAllingham <james.allingham@gmail.com>
2023-04-03 10:13:59 +0100

    Remove old code

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,53 +1,43 @@
-from src.models.livae import (
-    LIVAE,
-    make_livae_batch_loss,
-    make_livae_reconstruction_plot,
-    make_livae_sampling_plot,
-)
-from src.models.ivae import (
-    IVAE,
-    make_ivae_batch_loss,
-    make_ivae_reconstruction_plot,
-    make_ivae_sampling_plot,
-)
-from src.models.vae import (
-    VAE,
-    make_vae_batch_loss,
-    make_vae_reconstruction_plot,
-    make_vae_sampling_plot,
-)
+# from src.models.livae import (
+#     LIVAE,
+#     make_livae_batch_loss,
+#     make_livae_reconstruction_plot,
+#     make_livae_sampling_plot,
+# )
+# from src.models.vae import (
+#     VAE,
+#     make_vae_batch_loss,
+#     make_vae_reconstruction_plot,
+#     make_vae_sampling_plot,
+# )
 from src.models.ssil import (
     SSIL,
     make_ssil_batch_loss,
     make_ssil_reconstruction_plot,
     make_ssil_sampling_plot,
 )
-from src.models.ssilvae import (
-    SSILVAE,
-    make_ssilvae_batch_loss,
-    make_ssilvae_reconstruction_plot,
-    make_ssilvae_sampling_plot,
-)
+# from src.models.ssilvae import (
+#     SSILVAE,
+#     make_ssilvae_batch_loss,
+#     make_ssilvae_reconstruction_plot,
+#     make_ssilvae_sampling_plot,
+# )
 
 __all__ = [
-    "LIVAE",
-    "make_livae_batch_loss",
-    "make_livae_reconstruction_plot",
-    "make_livae_sampling_plot",
-    "IVAE",
-    "make_ivae_batch_loss",
-    "make_ivae_reconstruction_plot",
-    "make_ivae_sampling_plot",
-    "VAE",
-    "make_vae_batch_loss",
-    "make_vae_reconstruction_plot",
-    "make_vae_sampling_plot",
+    # "LIVAE",
+    # "make_livae_batch_loss",
+    # "make_livae_reconstruction_plot",
+    # "make_livae_sampling_plot",
+    # "VAE",
+    # "make_vae_batch_loss",
+    # "make_vae_reconstruction_plot",
+    # "make_vae_sampling_plot",
     "SSIL",
     "make_ssil_batch_loss",
     "make_ssil_reconstruction_plot",
     "make_ssil_sampling_plot",
-    "SSILVAE",
-    "make_ssilvae_batch_loss",
-    "make_ssilvae_reconstruction_plot",
-    "make_ssilvae_sampling_plot",
+    # "SSILVAE",
+    # "make_ssilvae_batch_loss",
+    # "make_ssilvae_reconstruction_plot",
+    # "make_ssilvae_sampling_plot",
 ]
