33e42528e49e8c948bc6075b2bb57c039f06f692
James Allingham <james.allingham@gmail.com>
2023-07-06 10:47:57 +0100

    Temporary SSILVAE script

--- experiments/train_ssilvae.py
+++ experiments/train_ssilvae.py
@@ -2,7 +2,7 @@ import os
 
 os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".45"
 
-import random
+import random as py_random
 import functools
 from absl import app
 from absl import flags
@@ -19,13 +19,14 @@ jnp.log(jnp.exp(1) - 1.0)
 
 import tensorflow as tf
 import flax
+
 # from flax.training import checkpoints
 import orbax.checkpoint
 from absl import logging
 
 from src.utils.training import setup_model, get_dataset_splits, train_loop
 import src.utils.input as input_utils
-from experiments.configs.ssilvae_mnist import get_config as  get_ssilvae_config
+from experiments.configs.ssilvae_mnist import get_config as get_ssilvae_config
 from experiments.configs.vae_mnist import get_config as get_vae_config
 
 
@@ -35,8 +36,8 @@ logging.set_verbosity(logging.INFO)
 FLAGS = flags.FLAGS
 flags.DEFINE_integer("seed", 0, "Random seed.")
 flags.DEFINE_integer("angle", 45, "Angle for rotation.")
-flags.DEFINE_integer("num_train", 50000, "Number of training examples.")
-flags.DEFINE_integer("train_steps", 7501, "Number of training steps.")
+flags.DEFINE_integer("num_trn", 50000, "Number of training examples.")
+flags.DEFINE_integer("total_steps", 7501, "Number of training steps.")
 flags.DEFINE_enum("wandb_mode", "online", ["online", "offline", "disabled"], "Mode for wandb.run.")
 flags.DEFINE_list("wandb_tags", [], "Tags for wandb.run.")
 flags.DEFINE_string("wandb_notes", "", "Notes for wandb.run.")
@@ -48,7 +49,7 @@ def random_word(length=5):
     consonants = "bcdfghjklmnpqrstvwxyz"
     vowels = "aeiou"
 
-    return "".join(random.choice((consonants, vowels)[i%2]) for i in range(length))
+    return "".join(py_random.choice((consonants, vowels)[i % 2]) for i in range(length))
 
 
 def main(argv):
@@ -58,15 +59,17 @@ def main(argv):
     rng = random.PRNGKey(FLAGS.seed)
     data_rng, model_rng = random.split(rng)
 
-    ssilvae_config = get_ssilvae_config(f"{FLAGS.angle},{FLAGS.num_train},{FLAGS.train_steps},1")
+    ssilvae_config = get_ssilvae_config(f"{FLAGS.angle},{FLAGS.num_trn},{FLAGS.total_steps},1")
     ssilvae_config.seed = FLAGS.seed
-    vae_config = get_vae_config(f"{FLAGS.angle},{FLAGS.num_train},{FLAGS.train_steps}")
+    vae_config = get_vae_config(f"{FLAGS.angle},{FLAGS.num_trn},{FLAGS.total_steps}")
     vae_config.seed = FLAGS.seed
 
-    train_ds, val_ds, _ = get_dataset_splits(vae_config, data_rng, vae_config.batch_size, vae_config.batch_size)
+    train_ds, val_ds, _ = get_dataset_splits(
+        vae_config, data_rng, vae_config.batch_size, vae_config.batch_size
+    )
 
-    vae_model, vae_state = setup_model(vae_config, model_rng, train_ds);
-    ssilvae_model, ssilvae_state = setup_model(ssilvae_config, model_rng, train_ds);
+    vae_model, vae_state = setup_model(vae_config, model_rng, train_ds)
+    ssilvae_model, ssilvae_state = setup_model(ssilvae_config, model_rng, train_ds)
 
     ssilvae_best_state, ssilvae_final_state = train_loop(
         ssilvae_config,
@@ -80,7 +83,7 @@ def main(argv):
             "notes": FLAGS.wandb_notes,
             "project": FLAGS.wandb_project,
             "entity": FLAGS.wandb_entity,
-        }
+        },
     )
 
     train_ds_1_epoch, valid_ds_1_epoch, _ = get_dataset_splits(vae_config, data_rng, 500, 500, 1)
@@ -106,11 +109,10 @@ def main(argv):
         )
 
     def modify_dataset(dataset_iter):
-
         modified_dataset = []
         for batch in dataset_iter:
             modified_images = get_proto(batch["image"])
-            batch['image'] = modified_images
+            batch["image"] = modified_images
             modified_dataset.append(batch)
 
         return modified_dataset
@@ -123,32 +125,44 @@ def main(argv):
     )
 
     def drop_mask(batch):
-        batch.pop('mask', None)
+        batch.pop("mask", None)
         return batch
 
-    valid_dataset = tf.data.Dataset.from_generator(
-        lambda: (batch for batch in modified_valid_data),
-        output_signature={
-            'image': tf.TensorSpec(shape=(1, 500, 28, 28, 1), dtype=tf.float32),  # image shape
-            'label': tf.TensorSpec(shape=(1, 500), dtype=tf.int32),    # label shape
-            'mask': tf.TensorSpec(shape=(1, 500), dtype=tf.float32)    # mask shape
-        }
-    ).unbatch().unbatch().filter(lambda x: x['mask'] == 1.).map(drop_mask)
+    valid_dataset = (
+        tf.data.Dataset.from_generator(
+            lambda: (batch for batch in modified_valid_data),
+            output_signature={
+                "image": tf.TensorSpec(shape=(1, 500, 28, 28, 1), dtype=tf.float32),  # image shape
+                "label": tf.TensorSpec(shape=(1, 500), dtype=tf.int32),  # label shape
+                "mask": tf.TensorSpec(shape=(1, 500), dtype=tf.float32),  # mask shape
+            },
+        )
+        .unbatch()
+        .unbatch()
+        .filter(lambda x: x["mask"] == 1.0)
+        .map(drop_mask)
+    )
 
-    train_dataset = tf.data.Dataset.from_generator(
-        lambda: (batch for batch in modified_train_data),
-        output_signature={
-            'image': tf.TensorSpec(shape=(1, 500, 28, 28, 1), dtype=tf.float32),  # image shape
-            'mask': tf.TensorSpec(shape=(1, 500), dtype=tf.float32)    # mask shape
-        }
-    ).unbatch().unbatch().filter(lambda x: x['mask'] == 1.).map(drop_mask)
+    train_dataset = (
+        tf.data.Dataset.from_generator(
+            lambda: (batch for batch in modified_train_data),
+            output_signature={
+                "image": tf.TensorSpec(shape=(1, 500, 28, 28, 1), dtype=tf.float32),  # image shape
+                "mask": tf.TensorSpec(shape=(1, 500), dtype=tf.float32),  # mask shape
+            },
+        )
+        .unbatch()
+        .unbatch()
+        .filter(lambda x: x["mask"] == 1.0)
+        .map(drop_mask)
+    )
 
     data_rng, train_ds_rng = jax.random.split(data_rng)
     train_ds_rng = jax.random.fold_in(train_ds_rng, jax.process_index())
 
     train_ds_aug = input_utils.get_data(
         dataset=train_dataset,
-        split='train',
+        split="train",
         rng=train_ds_rng,
         process_batch_size=vae_config.batch_size,
         preprocess_fn=None,
@@ -165,7 +179,7 @@ def main(argv):
 
     valid_ds_aug = input_utils.get_data(
         dataset=valid_dataset,
-        split='valid',  # type: ignore
+        split="valid",  # type: ignore
         rng=val_ds_rng,
         process_batch_size=vae_config.batch_size,
         preprocess_fn=None,
@@ -189,7 +203,7 @@ def main(argv):
             "notes": FLAGS.wandb_notes,
             "project": FLAGS.wandb_project,
             "entity": FLAGS.wandb_entity,
-        }
+        },
     )
 
 
