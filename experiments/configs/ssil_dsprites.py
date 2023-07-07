64c2db9375f4af6c849864568f06a8e6469df94f
JamesAllingham <james.allingham@gmail.com>
2023-07-07 14:37:43 +0100

    Fix ssil dsprites config

--- experiments/configs/ssil_dsprites.py
+++ experiments/configs/ssil_dsprites.py
@@ -6,22 +6,18 @@ from jax import numpy as jnp
 def get_config(params) -> config_dict.ConfigDict:
     config = config_dict.ConfigDict()
 
-    angle, num_trn, γ_mult = params.split(",")
-    config.angle = int(angle)
+    (num_trn,) = params.split(",")
     config.num_trn = int(num_trn)
     config.num_val = 10000
-    config.γ_mult = int(γ_mult)
-
-    config.seed = 0
+    config.num_tst = 10000
     # Dataset config
     config.dataset = "dsprites"
-    # config.data_dir = '~/data'
     config.shuffle_buffer_size = 50_000
-    config.repeat_after_batching = True  # NOTE: ordering of PP, shuffle, and repeat is important!
-    config.train_split = f"train[{config.num_val}:{config.num_val+config.num_trn}]"
-    config.pp_train = f'value_range(-1, 1)|random_rotate(-{config.angle}, {config.angle}, fill_value=-1)|keep(["image"])'
-    config.val_split = f"train[:{config.num_val}]"
-    config.pp_eval = f'value_range(-1, 1)|random_rotate(-{config.angle}, {config.angle}, fill_value=-1)|keep(["image", "label"])'
+    config.repeat_after_batch = True  # NOTE: ordering of PP and repeat is important!
+    config.train_split = f"train[{config.num_tst + config.num_val}:{config.num_tst + config.num_val + config.num_trn}]"
+    config.pp_train = f'value_range(-1, 1, 0, 1, True)|keep(["image"])'
+    config.val_split = f"train[{config.num_tst}:{config.num_tst + config.num_val}]"
+    config.pp_eval = f'value_range(-1, 1, 0, 1, True)|move_key("label_shape", "label")|keep(["image", "label"])'
 
     # Model config
     config.model_name = "SSIL"
@@ -33,15 +29,17 @@ def get_config(params) -> config_dict.ConfigDict:
     config.model.Η_given_Xhat.num_layers = 1
     config.model.Η_given_Xhat.num_bins = 5
     config.model.Η_given_Xhat.base = config_dict.ConfigDict()
-    config.model.Η_given_Xhat.base.dense_dims = (64, 32, 16)
+    config.model.Η_given_Xhat.base.dense_dims = (128,)
     config.model.Η_given_Xhat.base.conv_dims = ()
     config.model.Η_given_Xhat.base.dropout_rate = 0.0
     config.model.Η_given_Xhat.conditioner = config_dict.ConfigDict()
-    config.model.Η_given_Xhat.conditioner.hidden_dims = (256, 128)
+    config.model.Η_given_Xhat.conditioner.hidden_dims = (128,)
     config.model.Η_given_Xhat.conditioner.dropout_rate = 0.0
     config.model.Η_given_Xhat.trunk = config_dict.ConfigDict()
-    config.model.Η_given_Xhat.trunk.dense_dims = (256,)
-    config.model.Η_given_Xhat.trunk.conv_dims = (32, 64, 128)
+    config.model.Η_given_Xhat.trunk.dense_dims = (256, 128)
+    config.model.Η_given_Xhat.trunk.conv_dims = (16, 32, 64)
+    config.model.Η_given_Xhat.trunk.max_2strides = 2
+    config.model.Η_given_Xhat.trunk.resize = False
     config.model.Η_given_Xhat.trunk.dropout_rate = 0.0
     # p(η|X) config
     config.model.Η_given_X = copy.deepcopy(config.model.Η_given_Xhat)
@@ -53,7 +51,7 @@ def get_config(params) -> config_dict.ConfigDict:
 
     ## Optimizer config
     config.optim_name = "adamw"
-    config.learning_rate = 3e-4
+    config.learning_rate = 1e-4
     config.optim = config_dict.ConfigDict()
     config.optim.weight_decay = 1e-4
 
@@ -61,7 +59,7 @@ def get_config(params) -> config_dict.ConfigDict:
     config.lr_schedule_name = "warmup_cosine_decay_schedule"
     config.lr_schedule = config_dict.ConfigDict()
     config.lr_schedule.peak_value = 10 * config.learning_rate
-    config.lr_schedule.end_value = 1 * config.learning_rate
+    config.lr_schedule.end_value = 0.3 * config.learning_rate
     config.lr_schedule.decay_steps = config.total_steps
     config.lr_schedule.warmup_steps = config.total_steps // 10
 
@@ -69,11 +67,6 @@ def get_config(params) -> config_dict.ConfigDict:
     config.α = 1
 
     ## γ config
-    config.γ = 10 * config.γ_mult
-    config.γ_schedule_name = "cosine_decay_schedule"
-    config.γ_schedule = config_dict.ConfigDict()
-    config.γ_end_value = 1 * config.γ_mult
-    config.γ_schedule.alpha = config.γ_end_value / config.γ
-    config.γ_schedule.decay_steps = config.total_steps
+    config.γ = 0
 
     return config
