42fb2f88fca1289a4a149271ff2d8ef5f0b919cf
Bruno <bkmlodozeniec@gmail.com>
2023-09-19 09:49:37 +0100

    Add the evaluation of log-density of latents and speed-up sampling by ealuating probability of each dataset index

--- src/utils/datasets/augmented_dsprites.py
+++ src/utils/datasets/augmented_dsprites.py
@@ -1,6 +1,8 @@
 import enum
 from enum import StrEnum
-from typing import Callable, Mapping, NamedTuple, Protocol, Union
+import functools
+from typing import Callable, Iterable, Mapping, NamedTuple, Protocol, Union
+import jax
 
 import requests
 from pathlib import Path
@@ -70,10 +72,23 @@ class DspritesLatent(NamedTuple):
     label_shape: int
 
 
+ArrayLike = Union[np.ndarray, jnp.ndarray]
+
+
+class DspritesExamples(NamedTuple):
+    image: ArrayLike
+    label_shape: ArrayLike
+    value_scale: ArrayLike
+    value_orientation:ArrayLike
+    value_x_position:  ArrayLike
+    value_y_position: ArrayLike
+
+
+@functools.cache
 def construct_dsprites(
     root: PathLike,
     download: bool = True,
-) -> list[dict[str, np.ndarray]]:
+) -> DspritesExamples:
     """
     DSprites dataset from https://github.com/deepmind/dsprites-dataset with 3 classes (shapes).
 
@@ -94,22 +109,16 @@ def construct_dsprites(
     shapes = dataset_zip["latents_classes"][:, 1]
     latents = dataset_zip["latents_values"][:, 2:]
     scales, orientations, x_positions, y_positions = latents.T
+    return DspritesExamples(
+        image=imgs,
+        label_shape=shapes,
+        value_scale=scales,
+        value_orientation=orientations,
+        value_x_position=x_positions,
+        value_y_position=y_positions,
+    )
 
-    dataset_list = [
-        {
-            "image": img[:, :, None],  # Shape [64, 64, 1] (add channel dim.)
-            "label_shape": shape,
-            "value_scale": scale,
-            "value_orientation": orientation,
-            "value_x_position": x_position,
-            "value_y_position": y_position,
-        } for img, shape, scale, orientation, x_position, y_position in zip(
-            imgs, shapes, scales, orientations, x_positions, y_positions
-        )
-    ]
-    return dataset_list
-
-
+    
 def download_dsprites(filepath: PathLike):
     filepath = Path(filepath)
     # Download the dataset from https://github.com/deepmind/dsprites-dataset
@@ -126,87 +135,77 @@ def construct_augmented_dsprites(
     aug_dsprites_config: AugDspritesConfig,
     sampler_rng: random.PRNGKeyArray,
 ) -> tf.data.Dataset:
+    dataset_examples = construct_dsprites(root="data/dsprites", download=True)
 
-    dataset_list = construct_dsprites(root="data/dsprites", download=True)
-
-    # Make a mapping so that we can easily look up the index for a given
-    # configuration of augmentations:
-    latent_to_index: Mapping[DspritesLatent, int] = {
-        extract_latents_from_example(example): idx
-        for idx, example in enumerate(dataset_list)
-    }
-
-    # --- Get all the unique configurations of latents
-    # (all compositions of unique
-    # orientations/scales/... exist in DSprites, so just get the unique values
-    # for each latent variable):
-    dsprite_unique_orientations = set()
-    dsprite_unique_scales = set()
-    dsprite_unique_x_positions = set()
-    dsprite_unique_y_positions = set()
-    dsprite_unique_shapes = set()
-
-    for latent in latent_to_index.keys():
-        dsprite_unique_orientations.add(latent.value_orientation)
-        dsprite_unique_scales.add(latent.value_scale)
-        dsprite_unique_x_positions.add(latent.value_x_position)
-        dsprite_unique_y_positions.add(latent.value_y_position)
-        dsprite_unique_shapes.add(latent.label_shape)
-
-    dsprite_unique_orientations = sorted(dsprite_unique_orientations)
-    dsprite_unique_scales = sorted(dsprite_unique_scales)
-    dsprite_unique_x_positions = sorted(dsprite_unique_x_positions)
-    dsprite_unique_y_positions = sorted(dsprite_unique_y_positions)
-
-    # Function to retrieve the closest example in the dataset to a given latent (since we'll sample
-    # latents possibly inbetween values of the discrete domain of the dataset)
-    def get_closest_dsprites_example_given_latent(latent: DspritesLatent) -> int:
-        closest_latent = DspritesLatent(
-            value_orientation=get_closest_value_in_sorted_sequence(
-                latent.value_orientation, dsprite_unique_orientations
+    # --- Get the probability of each example's latent configuration under config:
+    @jax.jit
+    def latent_log_prob_for_config(
+        value_orientation: float,
+        value_scale: float,
+        value_x_position: float,
+        value_y_position: float,
+        label_shape: int,
+    ) -> float:
+        return latent_log_prob(
+            DspritesLatent(
+                value_orientation=value_orientation,
+                value_scale=value_scale,
+                value_x_position=value_x_position,
+                value_y_position=value_y_position,
+                label_shape=label_shape,
             ),
-            value_scale=get_closest_value_in_sorted_sequence(
-                latent.value_scale, dsprite_unique_scales
-            ),
-            value_x_position=get_closest_value_in_sorted_sequence(
-                latent.value_x_position, dsprite_unique_x_positions
-            ),
-            value_y_position=get_closest_value_in_sorted_sequence(
-                latent.value_y_position, dsprite_unique_y_positions
-            ),
-            label_shape=latent.label_shape,
+            config=aug_dsprites_config,
         )
-        return latent_to_index[closest_latent]
+    
+    log_probs = jax.vmap(latent_log_prob_for_config, in_axes=(0, 0, 0, 0, 0))(
+        dataset_examples.value_orientation,
+        dataset_examples.value_scale,
+        dataset_examples.value_x_position,
+        dataset_examples.value_y_position,
+        dataset_examples.label_shape,
+    )
+    # Convert to numpy and float64 for numerical stability:
+    log_probs = np.array(log_probs, dtype=np.float64)
+    norm_log_probs = log_probs - scipy.special.logsumexp(log_probs)
+    probs = np.exp(norm_log_probs)
 
-    # Get a generator that samples latents according to the distributions specified in the configuration:
-
-    latent_sampler: Callable[
-        [np.random.Generator], DspritesLatent
-    ] = get_dsprites_latent_sampler(aug_dsprites_config)
-
-    # Make a generator that returns examples with latents sampled from the latent_sampler:
-    def example_generator(rng: random.PRNGKey):
-        # Get a numpy rng
+    def example_idx_sampler(rng: random.PRNGKeyArray, num_batched_samples: int = 50000) -> Iterable[int]:
+        """
+        Sample an example index from the dataset with probability proportional to `probs`.
+        `num_batched_samples` helps with speed by vectorising the otherwise expenseive
+        `jax.random.choice` function.
+        """
         rng = np.random.default_rng(np.array(rng))
+        num_examples = len(probs)
         while True:
-            latent = latent_sampler(rng)
-            idx = get_closest_dsprites_example_given_latent(latent)
-            yield dataset_list[idx]
-
-    return tf.data.Dataset.from_generator(
-        example_generator,
-        output_signature=(
-            {
-                "image": tf.TensorSpec(shape=(64, 64, 1), dtype=tf.uint8),
-                "label_shape": tf.TensorSpec(shape=(), dtype=tf.int64),
-                "value_scale": tf.TensorSpec(shape=(), dtype=tf.float64),
-                "value_orientation": tf.TensorSpec(shape=(), dtype=tf.float64),
-                "value_x_position": tf.TensorSpec(shape=(), dtype=tf.float64),
-                "value_y_position": tf.TensorSpec(shape=(), dtype=tf.float64),
-            }
-        ),
+            yield from rng.choice(num_examples, size=num_batched_samples, p=probs)
+    
+    index_dataset = tf.data.Dataset.from_generator(
+        example_idx_sampler,
+        output_signature=tf.TensorSpec(shape=(), dtype=tf.int64),
         args=(sampler_rng,),
     )
+
+    dataset_examples_tf = DspritesExamples(
+        image=tf.convert_to_tensor(dataset_examples.image[..., None]),
+        label_shape=tf.convert_to_tensor(dataset_examples.label_shape),
+        value_scale=tf.convert_to_tensor(dataset_examples.value_scale),
+        value_orientation=tf.convert_to_tensor(dataset_examples.value_orientation),
+        value_x_position=tf.convert_to_tensor(dataset_examples.value_x_position),
+        value_y_position=tf.convert_to_tensor(dataset_examples.value_y_position),
+    )
+
+    @tf.function
+    def get_dataset_example(idx):
+        return {
+            "image": dataset_examples_tf.image[idx],
+            "label_shape": dataset_examples_tf.label_shape[idx],
+            "value_scale": dataset_examples_tf.value_scale[idx],
+            "value_orientation": dataset_examples_tf.value_orientation[idx],
+            "value_x_position": dataset_examples_tf.value_x_position[idx],
+            "value_y_position": dataset_examples_tf.value_y_position[idx],
+        }
+    return index_dataset.map(get_dataset_example, num_parallel_calls=tf.data.AUTOTUNE)
 
 
 def extract_latents_from_example(example: dict):
@@ -218,10 +217,130 @@ def extract_latents_from_example(example: dict):
         label_shape=int(example["label_shape"]),
     )
 
+
+def latent_log_prob(
+    latent: DspritesLatent,
+    config: AugDspritesConfig,
+) -> float:
+    config_per_shape: dict[int, ShapeDistributionConfig] = {
+        0: config.square_distribution,
+        1: config.ellipse_distribution,
+        2: config.heart_distribution,  # TODO check order is right
+    }
+
+    class LatentLogProbFuncPerShape(NamedTuple):
+        orientation: ScalarLogDensity
+        scale: ScalarLogDensity
+        x_position: ScalarLogDensity
+        y_position: ScalarLogDensity
+
+    log_probs_funcs_per_shape = {
+        shape: LatentLogProbFuncPerShape(
+            orientation=get_log_prob_func(shape_config.orientation),
+            scale=get_log_prob_func(shape_config.scale),
+            x_position=get_log_prob_func(shape_config.x_position),
+            y_position=get_log_prob_func(shape_config.y_position),
+        )
+        for shape, shape_config in config_per_shape.items()
+    }
+
+    log_prob_func_per_shape = {
+        shape: (
+            lambda x: (
+                log_probs_funcs.orientation(x.value_orientation)
+                + log_probs_funcs.scale(x.value_scale)
+                + log_probs_funcs.x_position(x.value_x_position)
+                + log_probs_funcs.y_position(x.value_y_position)
+                + jnp.log(1 / 3)  # Shape probability
+            )
+        )
+        for shape, log_probs_funcs in log_probs_funcs_per_shape.items()
+    }
+
+    return jax.lax.switch(
+        latent.label_shape,
+        [
+            log_prob_func_per_shape[0],
+            log_prob_func_per_shape[1],
+            log_prob_func_per_shape[2],
+        ],
+        latent,
+    )
+
+
+ScalarLogDensity = Callable[[float], float]
+
+
+def truncated_gaussian_log_prob(
+    loc: float,
+    scale: float,
+    minval: float,
+    maxval: float,
+) -> ScalarLogDensity:
+    return lambda x: jax.scipy.stats.truncnorm.logpdf(
+        x=x,
+        a=(minval - loc) / scale,
+        b=(maxval - loc) / scale,
+        loc=loc,
+        scale=scale,
+    )
+
+
+def mixture_log_prob(
+    prob_a: float,
+    component_a: ScalarLogDensity,
+    component_b: ScalarLogDensity,
+) -> ScalarLogDensity:
+    return lambda x: jnp.logaddexp(
+        jnp.log(prob_a) + component_a(x),
+        jnp.log(1 - prob_a) + component_b(x),
+    )
+
+
+def get_log_prob_func(
+    distribution_conf: DistributionConfig,
+) -> ScalarLogDensity:
+    kwargs = distribution_conf.kwargs
+    assert set(kwargs.keys()) == expected_kwargs_for_distribution_type(
+        distribution_conf.type
+    ), (
+        f"Expected kwargs {expected_kwargs_for_distribution_type(distribution_conf.type)} "
+        f"for distribution type {distribution_conf.type}, got {set(kwargs.keys())}"
+    )
+    match distribution_conf.type:
+        case DistributionType.UNIFORM:
+            return lambda x: jax.scipy.stats.uniform.logpdf(
+                x=x, loc=kwargs["low"], scale=kwargs["high"] - kwargs["low"]
+            )
+        case DistributionType.BIUNIFORM:
+            component_a = lambda x: jax.scipy.stats.uniform.logpdf(
+                x=x, loc=kwargs["low1"], scale=kwargs["high1"] - kwargs["low1"]
+            )
+            component_b = lambda x: jax.scipy.stats.uniform.logpdf(
+                x=x, loc=kwargs["low2"], scale=kwargs["high2"] - kwargs["low2"]
+            )
+            return lambda x: mixture_log_prob(
+                prob_a=0.5,
+                component_a=component_a,
+                component_b=component_b,
+            )(x)
+        case DistributionType.TRUNCATED_NORMAL:
+            return lambda x: truncated_gaussian_log_prob(
+                loc=kwargs["loc"],
+                scale=kwargs["scale"],
+                minval=kwargs["minval"],
+                maxval=kwargs["maxval"],
+            )(x)
+        case DistributionType.DELTA:
+            return lambda x: jnp.select(x == kwargs["value"], 0, -np.infty)
+        case _:
+            raise NotImplemented(f"Invalid distribution type {distribution_conf.type}")
+
+
 ScalarSampler = Callable[[np.random.Generator], float]
 
 
-def get_dsprites_latent_sampler(config: AugDspritesConfig):
+def get_dsprites_latent_sampler(config: AugDspritesConfig, size: int = 1):
     config_per_shape: dict[int, ShapeDistributionConfig] = {
         0: config.square_distribution,
         1: config.ellipse_distribution,
@@ -234,17 +353,20 @@ def get_dsprites_latent_sampler(config: AugDspritesConfig):
         x_position: ScalarSampler
         y_position: ScalarSampler
 
+    get_samp_fn_for_size = functools.partial(get_sample_func, size=size)
     sampler_per_shape = {
         shape: LatentSamplerPerShape(
-            orientation=get_sample_func(config.orientation),
-            scale=get_sample_func(config.scale),
-            x_position=get_sample_func(config.x_position),
-            y_position=get_sample_func(config.y_position),
-        ) for shape, config in config_per_shape.items()
+            orientation=get_samp_fn_for_size(config.orientation),
+            scale=get_samp_fn_for_size(config.scale),
+            x_position=get_samp_fn_for_size(config.x_position),
+            y_position=get_samp_fn_for_size(config.y_position),
+        )
+        for shape, config in config_per_shape.items()
     }
-    
-    shapes, probs = [0, 1, 2], [1/3, 1/3, 1/3]
-    shape_sampler: ScalarSampler = lambda rng: rng.choice(shapes, p=probs)
+
+    shapes, probs = [0, 1, 2], [1 / 3, 1 / 3, 1 / 3]
+    shape_sampler: ScalarSampler = lambda rng: rng.choice(shapes, p=probs, size=size)
+
     def sample(rng: np.random.Generator) -> DspritesLatent:
         return DspritesLatent(
             # Sample the label
@@ -260,7 +382,11 @@ def get_dsprites_latent_sampler(config: AugDspritesConfig):
 
 
 def truncated_gaussian(
-    loc: float, scale: float, minval: float, maxval: float,
+    loc: float,
+    scale: float,
+    minval: float,
+    maxval: float,
+    size: int = 1,
 ) -> ScalarSampler:
     distr = scipy.stats.truncnorm(
         a=(minval - loc) / scale,
@@ -270,6 +396,7 @@ def truncated_gaussian(
     )
     return lambda rng: distr.rvs(
         random_state=rng,
+        size=size,
     )
 
 
@@ -277,14 +404,27 @@ def mixture_sampler(
     prob_a: float,
     component_a: ScalarSampler,
     component_b: ScalarSampler,
+    size: int = 1,
 ) -> ScalarSampler:
     """Sample from a mixture of two distributions"""
     distr = scipy.stats.bernoulli(p=prob_a)
-    return lambda rng: component_a(rng) if distr.rvs(random_state=rng) else component_b(rng)
+    if size == 1:
+        return (
+            lambda rng: component_a(rng)
+            if distr.rvs(random_state=rng)
+            else component_b(rng)
+        )
+    else:
+        return lambda rng: np.where(
+            distr.rvs(random_state=rng, size=size),
+            component_a(rng),
+            component_b(rng),
+        )
 
 
 def get_sample_func(
     distribution_conf: DistributionConfig,
+    size: int = 1,
 ) -> Callable[[random.PRNGKeyArray], float]:
     kwargs = distribution_conf.kwargs
     assert set(kwargs.keys()) == expected_kwargs_for_distribution_type(
@@ -295,15 +435,25 @@ def get_sample_func(
     )
     match distribution_conf.type:
         case DistributionType.UNIFORM:
-            distr = scipy.stats.uniform(loc=kwargs["low"], scale=kwargs["high"] - kwargs["low"])
-            return lambda rng: distr.rvs(random_state=rng)
+            distr = scipy.stats.uniform(
+                loc=kwargs["low"], scale=kwargs["high"] - kwargs["low"]
+            )
+            return lambda rng: distr.rvs(random_state=rng, size=size)
         case DistributionType.BIUNIFORM:
-            component_a_distr = scipy.stats.uniform(loc=kwargs["low1"], scale=kwargs["high1"] - kwargs["low1"])
-            component_b_distr = scipy.stats.uniform(loc=kwargs["low2"], scale=kwargs["high2"] - kwargs["low2"])
+            component_a_distr = scipy.stats.uniform(
+                loc=kwargs["low1"], scale=kwargs["high1"] - kwargs["low1"]
+            )
+            component_b_distr = scipy.stats.uniform(
+                loc=kwargs["low2"], scale=kwargs["high2"] - kwargs["low2"]
+            )
             return mixture_sampler(
                 prob_a=0.5,
-                component_a = lambda rng: component_a_distr.rvs(random_state=rng),
-                component_b = lambda rng: component_b_distr.rvs(random_state=rng),
+                component_a=lambda rng: component_a_distr.rvs(
+                    random_state=rng, size=size
+                ),
+                component_b=lambda rng: component_b_distr.rvs(
+                    random_state=rng, size=size
+                ),
             )
         case DistributionType.TRUNCATED_NORMAL:
             return truncated_gaussian(
@@ -311,8 +461,13 @@ def get_sample_func(
                 scale=kwargs["scale"],
                 minval=kwargs["minval"],
                 maxval=kwargs["maxval"],
+                size=size,
             )
         case DistributionType.DELTA:
-            return lambda rng: kwargs["value"]
+            return (
+                lambda rng: kwargs["value"]
+                if size == 1
+                else np.full(size, kwargs["value"])
+            )
         case _:
             raise NotImplemented(f"Invalid distribution type {distribution_conf.type}")
