ad3a15fd31838d94bc242fc8f5470a5e08695a05
James Allingham <james.allingham@gmail.com>
2023-09-28 09:24:39 +0100

    Add a new "full_transform_image" function which does affine + hsv

--- src/transformations/__init__.py
+++ src/transformations/__init__.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from chex import Array, assert_shape, assert_rank
+from chex import Array, assert_rank, assert_shape
 
 from src.transformations.affine import affine_transform_image, rotate_image
 from src.transformations.color import (
@@ -17,6 +17,7 @@ __all__ = [
     "hue_transform_image",
     "rgb_transform_image",
     "transform_image",
+    "full_transform_image",
 ]
 
 
@@ -46,12 +47,47 @@ def transform_image(
     assert_shape(η, (5,))
 
     # Apply affine transformations
-    η_affine = jnp.concatenate((η[:5], jnp.zeros(1, dtype=η.dtype)))
+    η_affine = jnp.concatenate((η, jnp.zeros(1, dtype=η.dtype)))
     image = affine_transform_image(
         image, η_affine, fill_mode=fill_mode, fill_value=fill_value, order=order,
     )
 
-    # # Apply color transformations
-    # image = hue_transform_image(image, η[5:6])
+    return image
+
+
+def full_transform_image(
+    image: Array,
+    η: Array,
+    fill_mode: str = "constant",
+    fill_value: float = -1.0,
+) -> Array:
+    """Applies transformations to an image.
+
+    Args:
+        image: a rank-3 Array of shape (height, width, num channels) – i.e. Jax/TF image format.
+
+        η: an Array with 5 entries:
+        * η_0 controls translation in x.
+        * η_1 controls translation in y.
+        * η_2 is the angle of rotation.
+        * η_3 is the scaling factor in x.
+        * η_4 is the scaling factor in y.
+        * η_5 is the shearing.
+        * η_6 is the hue.
+        * η_7 is the saturation.
+
+    Returns:
+        A transformed image of same shape as the input.
+    """
+    assert_rank(image, 3)
+    assert_shape(η, (8,))
+
+    # Apply affine transformations
+    image = affine_transform_image(
+        image, η[:6], fill_mode=fill_mode, fill_value=fill_value
+    )
+
+    # Apply color transformations
+    image = hsv_transform_image(image, η[6:])
 
     return image
