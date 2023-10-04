20ff69f31963cda5ce77590cc94094beea8b0a74
Bruno <bkmlodozeniec@gmail.com>
2023-10-04 10:16:37 +0100

    Update dsprites config with changes to code and a simpler augment distribution

--- experiments/configs/pgm_dsprites.py
+++ experiments/configs/pgm_dsprites.py
@@ -17,8 +17,8 @@ def get_config() -> config_dict.ConfigDict:
     config.train_split = ""  # Doesn't matter for augmentedDsprites
     config.val_split = ""  # Doesn't matter for augmentedDsprites
 
-    config.pp_train = f'value_range(-1, 1)|keep(["image", "mask"])'
-    config.pp_eval = f'value_range(-1, 1)|keep(["image", "mask", "label_shape"])'
+    config.pp_train = f'value_range(-1, 1, 0, 1)|move_key("label_shape", "label")|keep(["image", "label"])'
+    config.pp_eval = f'value_range(-1, 1, 0, 1)|move_key("label_shape", "label")|keep(["image", "mask", "label"])'
 
     # Dsprites specific settings:
     config.aug_dsprites = config_dict.ConfigDict()
@@ -26,15 +26,15 @@ def get_config() -> config_dict.ConfigDict:
     config.aug_dsprites.square_distribution = config_dict.ConfigDict()
 
     config.aug_dsprites.square_distribution.orientation = DistributionConfig(
-        "uniform", {"low": 0.0, "high": math.pi / 4}
+        "uniform", {"low": 0.0, "high": math.pi / 2}
     )
     config.aug_dsprites.square_distribution.scale = DistributionConfig(
         "uniform",
-        {"low": 0.8, "high": 1.0},
+        {"low": 0.65, "high": 1.0},
     )
     config.aug_dsprites.square_distribution.x_position = DistributionConfig(
         "biuniform",
-        {"low1": 0.2, "high1": 0.6, "low2": 0.4, "high2": 0.8},
+        {"low1": 0.15, "high1": 0.6, "low2": 0.4, "high2": 0.9},
     )
     config.aug_dsprites.square_distribution.y_position = DistributionConfig(
         "truncated_normal",
@@ -43,13 +43,13 @@ def get_config() -> config_dict.ConfigDict:
 
     config.aug_dsprites.ellipse_distribution = config_dict.ConfigDict()
     config.aug_dsprites.ellipse_distribution = (
-        config.aug_dsprites.square_distribution
+        config.aug_dsprites.square_distribution.copy_and_resolve_references()
     )  # Same distributions
 
     config.aug_dsprites.heart_distribution = config_dict.ConfigDict()
 
     config.aug_dsprites.heart_distribution.orientation = DistributionConfig(
-        "uniform", {"low": 0.0, "high": 2 * math.pi}
+        "uniform", {"low": 0.0, "high": math.pi / 2}
     )
     config.aug_dsprites.heart_distribution.scale = DistributionConfig(
         "uniform",
@@ -57,7 +57,7 @@ def get_config() -> config_dict.ConfigDict:
     )
     config.aug_dsprites.heart_distribution.x_position = DistributionConfig(
         "uniform",
-        {"low": 0.2, "high": 0.8},
+        {"low": 0.1, "high": 0.8},
     )
     config.aug_dsprites.heart_distribution.y_position = DistributionConfig(
         "biuniform",
@@ -68,35 +68,59 @@ def get_config() -> config_dict.ConfigDict:
             "high2": 0.8,
         },
     )
+    # Shape probabilities:
+    config.aug_dsprites.heart_distribution.shape_prob = 1 / 3
+    config.aug_dsprites.square_distribution.shape_prob = 1 / 3
+    config.aug_dsprites.ellipse_distribution.shape_prob = 1 / 3
 
     # --- Training ---
     config.n_samples = 5
     config.eval_freq = 0.01
     config.difficulty_weighted_inf_loss = False
+    config.symmetrised_samples_in_loss = True
+    config.interpolation_order = 1
 
     config.gen_steps = 10_000
     config.gen_init_lr = 1e-4
     config.gen_peak_lr_mult = 3
     config.gen_final_lr_mult = 1 / 30
     config.gen_warmup_steps = 1_000
-    config.σ_lr = 1e-2
-    config.inf_steps = 10_000
-    config.inf_init_lr = 3e-4
-    config.inf_peak_lr_mult = 9
-    config.inf_final_lr_mult = 1 / 300
-    config.inf_warmup_steps = 1_000
 
-    config.α_schedule_pct = 0.25
-    config.α_schedule_final_value = 0.0
-    config.β_schedule_pct = 1.0
-    config.β_schedule_final_value = 0.99
+    config.σ_lr = 1e-2
+    config.inf_steps = 30_000
+    config.inf_lr = 4e-3
+    config.inf_init_lr_mult = 1e-3
+    config.inf_final_lr_mult = 1e-1
+    config.inf_warmup_steps = 3_000  # roughly 10% of training
+
+    config.inf_weight_decay = 0.05
+
+    config.η_loss_mult_peak = 1.0
+    config.η_loss_decay_start = 1.0
+    config.η_loss_decay_end = 1.0
+    config.augment_warmup_end = 0.7
+
+    config.x_mse_loss_mult = 1.0
+    config.invertibility_loss_mult = 1.0
+
+    # Separated out model bounds, and augmentation bounds for augmentations sampled for the
+    # training objective:
+    config.augment_bounds = (.5, .5, jnp.pi, .5, .5)
+    config.augment_offset = (0.0, 0.0, 0.0, 0.0, 0.0)
 
     config.model = config_dict.ConfigDict()
-    config.model.bounds = (0.25, 0.25, jnp.pi, 0.25, 0.25)
-    config.model.offset = (0.0, 0.0, 0.0, 0.0, 0.0)
     config.model.inference = config_dict.ConfigDict()
-    config.model.inference.hidden_dims = (2048, 1024, 512, 128)
+    config.model.inference.model_type = "mlp"
+    # config.model.inference.model_type = "convnext"
+    # config.model.inference.convnext_type = "tiny"
+    config.model.inference.bounds = (0.5, 0.5, jnp.pi, 0.5, 0.5)
+    config.model.inference.offset = (0.0, 0.0, 0.0, 0.0, 0.0)
+    config.model.inference.hidden_dims = (4096, 2048, 512, 128)
+    config.model.inference.squash_to_bounds = False
+
     config.model.generative = config_dict.ConfigDict()
+    config.model.generative.bounds = (0.25, 0.25, jnp.pi, 0.25, 0.25)
+    config.model.generative.offset = (0.0, 0.0, 0.0, 0.0, 0.0)
     config.model.generative.hidden_dims = (1024, 512, 256)
     config.model.generative.num_flows = 2
     config.model.generative.num_bins = 4
