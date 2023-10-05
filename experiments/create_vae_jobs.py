abc87300b105bd04b6ebfe934260779a0bf4fd9d
James Allingham <james.allingham@gmail.com>
2023-10-05 12:07:45 +0100

    Add sweep of num_trn, init_lr, steps

--- experiments/create_vae_jobs.py
+++ experiments/create_vae_jobs.py
@@ -1,27 +1,28 @@
-from hashlib import md5
 from itertools import product
 from pathlib import Path
 
 ################################### Configs #####################################
 
-EXPERIMENT_NAME = "test_vae_sweep"
+EXPERIMENT_NAME = "vae_sweep"
 JOBS_FOLDER = f"jobs_{EXPERIMENT_NAME}"
 DELETE_PREV_FOLDER = True
 # Set ^ to False if adding new jobs to the experiment while it is still running.
 # Deleting a jobs folder while it is running will cause it to fail.
 SCRIPT = "train_vae.py"
-# RESULTS_FOLDER = f'~/rds/rds-t2-cs133-hh9aMiOkJqI/jua23/blt/icml_results/small_scale/{EXPERIMENT_NAME}'
 CONFIG_NAMES = [
     "vae_mnist",
 ]
-ANGLES = [0, 1, 5, 15, 30, 45, 90, 135, 180, 270, 360]
-RANDOM_SEEDS = [0, 1, 2]
-FLAGS_TO_ADD = ["--wandb_tags=pgm,mnist,angle_sweep"]
+ANGLES = [0, 45, 90, 180, 360]  # [0, 1, 5, 15, 30, 45, 90, 135, 180, 270, 360]
+RANDOM_SEEDS = [0]
+NUM_TRNS = [5_000, 15_000, 50_000]
+INIT_LRS = [3e-4, 1e-4]
+STEPS = [1_000, 3_000, 10_000]
+FLAGS_TO_ADD = ["--wandb_tags=vae,mnist,angle_sweep,num_trn_sweep"]
 
 ################################################################################
 
 times = {
-    "pgm_sweep": "00:30:00",
+    "vae_mnist": "01:00:00",
 }
 
 jobsfolder = Path(f"./{JOBS_FOLDER}")
@@ -38,15 +39,17 @@ for config_name in CONFIG_NAMES:
     jobsfile.touch()
 
     with open(jobsfile, "w") as f:
-        for angle, random_seed in product(ANGLES, RANDOM_SEEDS):
+        for angle, random_seed, num_trn, init_lr, steps in product(
+            ANGLES, RANDOM_SEEDS, NUM_TRNS, INIT_LRS, STEPS
+        ):
             for flags_to_add in FLAGS_TO_ADD:
-                # unique_hash = md5("".join(sorted(flags_to_add)).strip().encode()).hexdigest()
-
                 line = (
                     f"{SCRIPT} "
-                    f"--config configs/{config_name}.py:{angle} "
+                    f"--config configs/{config_name}.py:{angle},{num_trn} "
                     f"--config.seed {random_seed} "
-                    # f'--config.results_folder {RESULTS_FOLDER}/{unique_hash} '
+                    f"--config.init_lr {init_lr} "
+                    f"--config.steps {steps} "
+                    f"--config.warmup_steps {steps // 10}"
                     f" {flags_to_add}"
                 )
                 f.write(line + "\n")
