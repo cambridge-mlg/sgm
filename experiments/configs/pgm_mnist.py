e5b064751c5d930c947cba403c13755dcb760ab0
JamesAllingham <james.allingham@gmail.com>
2023-10-09 09:25:02 +0100

    Allow float angles, set warmup steps to be 10%

--- experiments/configs/pgm_mnist.py
+++ experiments/configs/pgm_mnist.py
@@ -6,7 +6,7 @@ def get_config(params) -> config_dict.ConfigDict:
     config = config_dict.ConfigDict()
 
     params = params.split(",")
-    config.angle = int(params[0])
+    config.angle = float(params[0])
     config.num_val = 10000
     if len(params) > 1:
         config.num_trn = int(params[1])
@@ -31,13 +31,13 @@ def get_config(params) -> config_dict.ConfigDict:
     config.inf_init_lr = 1e-4
     config.inf_peak_lr_mult = 3
     config.inf_final_lr_mult = 1 / 30
-    config.inf_warmup_steps = 1_000
+    config.inf_warmup_steps = config.inf_steps // 10
     config.σ_lr = 1e-2
     config.gen_steps = 10_000
     config.gen_init_lr = 3e-4
     config.gen_peak_lr_mult = 9
     config.gen_final_lr_mult = 1 / 300
-    config.gen_warmup_steps = 1_000
+    config.gen_warmup_steps = config.gen_steps // 10
 
     config.α_schedule_pct = 0.25
     config.α_schedule_final_value = 0.0
