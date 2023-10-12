a5d384c4d26e558d70883b5cf69ad1a9d0333d28
JamesAllingham <james.allingham@gmail.com>
2023-10-12 18:35:54 +0100

    tmp commit

--- src/models/transform_generative_model.py
+++ src/models/transform_generative_model.py
@@ -165,28 +165,32 @@ def make_transformation_generative_train_and_eval(
 
         def get_xhat_on_random_augmentation(x, rng):
             """
-            Rather than obtaining the prototype of `x` directly by passing it into the prototype 
+            Rather than obtaining the prototype of `x` directly by passing it into the prototype
             function, augment it randomly and then pass it through to get the prototype.
 
-            This is useful for training the generative model to be robust to imperfections in the 
+            This is useful for training the generative model to be robust to imperfections in the
             canonicalization function.
             """
             sample_rng, prototype_fn_rng = random.split(rng)
             Η_rand = distrax.Uniform(
                 # Separate model bounds and augment bounds
-                low=-jnp.array(config.augment_bounds) + jnp.array(config.augment_offset),
-                high=jnp.array(config.augment_bounds) + jnp.array(config.augment_offset),
+                low=-jnp.array(config.augment_bounds)
+                + jnp.array(config.augment_offset),
+                high=jnp.array(config.augment_bounds)
+                + jnp.array(config.augment_offset),
             )
             η_rand = Η_rand.sample(seed=sample_rng, sample_shape=())
             η_rand_aff_mat = gen_affine_matrix_no_shear(η_rand)
 
             x_rand = transform_image_with_affine_matrix(x, η_rand_aff_mat)
-            
+
             η_rand_proto = prototype_function(x_rand, prototype_fn_rng)
             η_rand_proto_aff_mat = gen_affine_matrix_no_shear(η_rand_proto)
             η_rand_proto_aff_mat_inv = jnp.linalg.inv(η_rand_proto_aff_mat)
             return transform_image_with_affine_matrix(
-                x, η_rand_aff_mat @ η_rand_proto_aff_mat_inv, order=config.interpolation_order
+                x,
+                η_rand_aff_mat @ η_rand_proto_aff_mat_inv,
+                order=config.interpolation_order,
             )
 
         def per_sample_loss_fn(rng):
