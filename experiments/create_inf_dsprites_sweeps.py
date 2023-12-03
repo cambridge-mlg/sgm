82da6a51fc60709a57c389e966530d9ce6341316
James Allingham <james.allingham@gmail.com>
2023-12-03 16:13:15 +0000

    Add a new bayesian sweep for the inference nets

--- experiments/create_inf_dsprites_sweeps.py
+++ experiments/create_inf_dsprites_sweeps.py
@@ -9,19 +9,20 @@ from experiments.utils import format_thousand
 ENTITY = "invariance-learners"
 PROJECT = "iclr2024experiments"
 MAX_NUM_RUNS = 32
-SWEEP_CONFIG = "inf_hyper_sweep.yaml"
+SWEEP_TYPE = "bayes"  # "rand"
+SWEEP_CONFIG = f"inf_{SWEEP_TYPE}_hyper_sweep.yaml"
 
 
 parent_path = Path(__file__).parent
 sweep_path = parent_path / SWEEP_CONFIG
 
-job_folder = parent_path / f"jobs_inf_rand_dsprites_sweep"
+job_folder = parent_path / f"jobs_inf_{SWEEP_TYPE}_dsprites_sweep"
 job_folder.mkdir(exist_ok=True)
 
 with sweep_path.open() as file:
     sweep_config = yaml.safe_load(file)
 
-sweep_name = f"inf_rand_dsprites_sweep"
+sweep_name = f"inf_{SWEEP_TYPE}_dsprites_sweep"
 print(sweep_name)
 sweep_config["name"] = sweep_name
 sweep_config["command"][2] = f"--config=experiments/configs/inf_dsprites.py"
@@ -30,7 +31,7 @@ sweep_config["run_cap"] = MAX_NUM_RUNS
 
 sweep_id = wandb.sweep(sweep_config, entity=ENTITY, project=PROJECT)
 
-job_file = job_folder / f"{sweep_name}%01:30:00.txt"
+job_file = job_folder / f"{sweep_name}%01:00:00.txt"
 if job_file.exists():
     job_file.unlink()
 with job_file.open("w") as job_file:
--- experiments/create_inf_mnist_sweeps.py
+++ experiments/create_inf_mnist_sweeps.py
@@ -9,22 +9,23 @@ from experiments.utils import format_thousand
 ENTITY = "invariance-learners"
 PROJECT = "iclr2024experiments"
 MAX_NUM_RUNS = 32
-SWEEP_CONFIG = "inf_hyper_sweep.yaml"
-ANGLES = [0, 90, 180]
+ANGLES = [0]
 NUM_TRNS = [25_000, 50_000]
+SWEEP_TYPE = "bayes"  # "rand"
+SWEEP_CONFIG = f"inf_{SWEEP_TYPE}_hyper_sweep.yaml"
 
 
 parent_path = Path(__file__).parent
 sweep_path = parent_path / SWEEP_CONFIG
 
-job_folder = parent_path / f"jobs_inf_rand_mnist_sweep"
+job_folder = parent_path / f"jobs_inf_{SWEEP_TYPE}_mnist_sweep"
 job_folder.mkdir(exist_ok=True)
 
 for angle, num_trn in product(ANGLES, NUM_TRNS):
     with sweep_path.open() as file:
         sweep_config = yaml.safe_load(file)
 
-    sweep_name = f"inf_rand_mnist_sweep_{angle:03}_{format_thousand(num_trn)}"
+    sweep_name = f"inf_{SWEEP_TYPE}_mnist_sweep_{angle:03}_{format_thousand(num_trn)}"
     print(sweep_name)
     sweep_config["name"] = sweep_name
     sweep_config["command"][
@@ -35,7 +36,7 @@ for angle, num_trn in product(ANGLES, NUM_TRNS):
 
     sweep_id = wandb.sweep(sweep_config, entity=ENTITY, project=PROJECT)
 
-    job_file = job_folder / f"{sweep_name}%01:30:00.txt"
+    job_file = job_folder / f"{sweep_name}%01:00:00.txt"
     if job_file.exists():
         job_file.unlink()
     with job_file.open("w") as job_file:
