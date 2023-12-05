e4003fabb5a75603bdb0ff0110304b1f072263e9
James Allingham <james.allingham@gmail.com>
2023-12-05 11:15:14 +0000

    Add grid sweep

--- experiments/create_inf_mnist_sweeps.py
+++ experiments/create_inf_mnist_sweeps.py
@@ -8,24 +8,28 @@ from experiments.utils import format_thousand
 
 ENTITY = "invariance-learners"
 PROJECT = "icml2024"
-MAX_NUM_RUNS = 32
+MAX_NUM_RUNS = 144
 ANGLES = [0]
-NUM_TRNS = [25_000, 50_000]
-SWEEP_TYPE = "bayes"  # "rand"
+NUM_TRNS = [
+    # 25_000,
+    # 37_500,
+    50_000,
+]
+SWEEP_TYPE = "grid"  # "rand" or "bayes"
 SWEEP_CONFIG = f"inf_{SWEEP_TYPE}_hyper_sweep.yaml"
 
 
 parent_path = Path(__file__).parent
 sweep_path = parent_path / SWEEP_CONFIG
 
-job_folder = parent_path / f"jobs_inf_{SWEEP_TYPE}_mnist_sweep"
+job_folder = parent_path / f"jobs_inf_{SWEEP_TYPE}_mnist_sweep_seed1"
 job_folder.mkdir(exist_ok=True)
 
 for angle, num_trn in product(ANGLES, NUM_TRNS):
     with sweep_path.open() as file:
         sweep_config = yaml.safe_load(file)
 
-    sweep_name = f"inf_{SWEEP_TYPE}_mnist_sweep_{angle:03}_{format_thousand(num_trn)}"
+    sweep_name = f"inf_{SWEEP_TYPE}_mnist_sweep_{angle:03}_{format_thousand(num_trn)}_1"
     print(sweep_name)
     sweep_config["name"] = sweep_name
     sweep_config["command"][
@@ -36,7 +40,7 @@ for angle, num_trn in product(ANGLES, NUM_TRNS):
 
     sweep_id = wandb.sweep(sweep_config, entity=ENTITY, project=PROJECT)
 
-    job_file = job_folder / f"{sweep_name}%01:00:00.txt"
+    job_file = job_folder / f"{sweep_name}%01:30:00.txt"
     if job_file.exists():
         job_file.unlink()
     with job_file.open("w") as job_file:
