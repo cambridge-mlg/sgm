ce9cc3eea62b33a580645b96b0e10254b7d0a78a
JamesAllingham <james.allingham@gmail.com>
2023-12-20 00:20:49 +0000

    Bugfix for incorrect test batch size

--- src/utils/input.py
+++ src/utils/input.py
@@ -87,10 +87,10 @@ def get_data(
 
     num_test_examples = dataset_builder.info.splits[test_split].num_examples
     # Compute how many batches we need to contain the entire test set.
-    pad_up_to_batches = int(jnp.ceil(num_test_examples / config.batch_size))
+    pad_up_to_batches = int(jnp.ceil(num_test_examples / batch_size_eval))
 
     test_ds = deterministic_data.create_dataset(
-        dataset_builder,
+        dataset_or_builder,
         split=tfds.split_for_jax_process(test_split),
         rng=test_rng,
         batch_dims=[jax.local_device_count(), local_batch_size_eval],
