7d3c97303af5b9bc768b6bc5fc6d24010293cffe
James Allingham <james.allingham@gmail.com>
2024-01-05 20:24:08 +0000

    Add invae option

--- experiments/sweeps/create_vae_augvae_sweeps.py
+++ experiments/sweeps/create_vae_augvae_sweeps.py
@@ -9,21 +9,21 @@ from experiments.utils import format_thousand
 ENTITY = "invariance-learners"
 PROJECT = "icml2024"
 CHECKPOINT_DIR = "/home/jua23/rds/hpc-work/learning-invariances-models"
-MAX_NUM_RUNS = 288
+MAX_NUM_RUNS = 144
 ANGLES = [
     0,
-    # 15,
-    # 90,
-    # 180,
+    15,
+    90,
+    180,
 ]
 NUM_TRNS = [
     50_000,
-    # 37_500,
-    # 25_000,
+    37_500,
+    25_000,
 ]
 MODEL_NAMES = [
-    "augvae",
-    # "invvae"
+    # "augvae",
+    "invvae",
     # "vae",
 ]
 SEEDS = [
@@ -38,10 +38,14 @@ parent_path = Path(__file__).parent
 sweep_path = parent_path / SWEEP_CONFIG
 
 for model_name in MODEL_NAMES:
-    job_folder = parent_path.parent / "jobs" / f"{model_name}_{SWEEP_TYPE}_sweep"
+    job_folder = parent_path.parent / "jobs" / f"{model_name}_{SWEEP_TYPE}_sweep2"
     job_folder.mkdir(exist_ok=True)
 
     for angle, num_trn, seed in product(ANGLES, NUM_TRNS, SEEDS):
+        # match (model_name, angle, num_trn, seed):
+        #     case ("vae" | "augvae", 0 | 180, 50_000 | 25_000, 0):
+        #         continue
+
         with sweep_path.open() as file:
             sweep_config = yaml.safe_load(file)
 
@@ -49,33 +53,35 @@ for model_name in MODEL_NAMES:
         print(sweep_name)
         sweep_config["name"] = sweep_name
 
-        if model_name == "augvae":
+        if model_name in ["augvae", "invvae"]:
             sweep_config["command"][
                 2
             ] = f"--vae_config=experiments/configs/vae_mnist.py:{angle},{num_trn}"
             sweep_config["command"].insert(
                 3,
-                f"--inf_config=experiments/configs/inf_best.py:MNIST,{seed},0,{num_trn}",
+                f"--inf_config=experiments/configs/inf_best.py:MNIST,{seed},{angle},{num_trn}",
             )
             sweep_config["command"].insert(
                 3,
-                f"--gen_config=experiments/configs/gen_best.py:MNIST,{seed},0,{num_trn}",
+                f"--gen_config=experiments/configs/gen_best.py:MNIST,{seed},{angle},{num_trn}",
             )
             sweep_config["command"].append(
-                f"--inf_config.checkpoint={CHECKPOINT_DIR}/inf_best_ckpt_MNIST_{seed}_0_{num_trn}"
+                f"--inf_config.checkpoint={CHECKPOINT_DIR}/inf_best_ckpt_MNIST_{seed}_{angle}_{num_trn}"
             )
             sweep_config["command"].append(
-                f"--gen_config.checkpoint={CHECKPOINT_DIR}/gen_best_ckpt_MNIST_{seed}_0_{num_trn}"
+                f"--gen_config.checkpoint={CHECKPOINT_DIR}/gen_best_ckpt_MNIST_{seed}_{angle}_{num_trn}"
             )
+            sweep_config["program"] = f"experiments/train/train_{model_name}.py"
             sweep_config["command"].append(f"--vae_config.seed={seed}")
-            # sweep_config["command"].append(f"--vae_config.test_split=test")
-            sweep_config["program"] = "experiments/train/train_augvae.py"
+            sweep_config["command"].append(f"--vae_config.test_split=test")
         else:
             sweep_config["command"][
                 2
             ] = f"--config=experiments/configs/vae_mnist.py:{angle},{num_trn}"
+            sweep_config["command"].append(f"--config.seed={seed}")
+            sweep_config["command"].append(f"--config.test_split=test")
 
-        sweep_config["run_cap"] = MAX_NUM_RUNS
+        # sweep_config["run_cap"] = MAX_NUM_RUNS
 
         sweep_id = wandb.sweep(sweep_config, entity=ENTITY, project=PROJECT)
 
