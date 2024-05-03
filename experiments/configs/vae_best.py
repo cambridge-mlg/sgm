ce91d336ad44dfc07f6589878a4fec8f6163bedc
James Allingham <james.allingham@gmail.com>
2024-05-03 14:02:04 +0100

    Updates for GalaxyMNIST

--- experiments/configs/vae_best.py
+++ experiments/configs/vae_best.py
@@ -1,7 +1,7 @@
 from jax import numpy as jnp
 from ml_collections import config_dict
 
-from experiments.configs.datasets import add_mnist_config
+from experiments.configs.datasets import add_galaxy_mnist_config, add_mnist_config
 
 
 def get_config(params) -> config_dict.ConfigDict:
@@ -9,12 +9,14 @@ def get_config(params) -> config_dict.ConfigDict:
 
     params = params.split(",")
     config.model_name = params[0]
-    if len(params) > 1:
-        config.angle = int(params[1])
-        config.num_trn = int(params[2])
-
-    config.seed = 0
-    config.dataset = "MNIST"
+    config.dataset = params[1]
+    assert config.dataset in ["MNIST", "galaxy_mnist"]
+    config.seed = int(params[2])
+    if len(params) == 4:
+        config.num_trn = int(params[3])
+    if len(params) > 4:
+        config.angle = float(params[3])
+        config.num_trn = int(params[4])
 
     config.model = config_dict.ConfigDict()
     config.model.latent_dim = 20
@@ -102,19 +104,88 @@ def get_config(params) -> config_dict.ConfigDict:
             config.model.conv_dims = (64, 128)
             config.steps = 10_000
             config.warmup_steps_pct = 0.2
+        case ("AugVAE", "galaxy_mnist", None, 7_000, 0):  # kes7wvpk
+            config.lr = 6e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 20_000
+            config.warmup_steps_pct = 0.15
+        case ("AugVAE", "galaxy_mnist", None, 7_000, 1):  # 0anwaceg
+            config.lr = 3e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 20_000
+            config.warmup_steps_pct = 0.15
+        case ("AugVAE", "galaxy_mnist", None, 7_000, 2):  # ee7alk3d
+            config.lr = 3e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 20_000
+            config.warmup_steps_pct = 0.15
+        case ("AugVAE", "galaxy_mnist", None, 3_500, 0):  # o6zyqudm
+            config.lr = 3e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 20_000
+            config.warmup_steps_pct = 0.2
+        case ("AugVAE", "galaxy_mnist", None, 3_500, 1):  # rib9113j
+            config.lr = 3e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 20_000
+            config.warmup_steps_pct = 0.15
+        case ("AugVAE", "galaxy_mnist", None, 3_500, 2):  # f0ud74ak
+            config.lr = 6e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 20_000
+            config.warmup_steps_pct = 0.15
+        case ("VAE", "galaxy_mnist", None, 7_000, 0):  # 2o0gjo8q
+            config.lr = 9e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 5_000
+            config.warmup_steps_pct = 0.15
+        case ("VAE", "galaxy_mnist", None, 7_000, 1):  # 0k0blwtl
+            config.lr = 3e-3
+            config.model.conv_dims = (64, 128)
+            config.steps = 5_000
+            config.warmup_steps_pct = 0.2
+        case ("VAE", "galaxy_mnist", None, 7_000, 2):  # kubf9fsa
+            config.lr = 9e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 5_000
+            config.warmup_steps_pct = 0.2
+        case ("VAE", "galaxy_mnist", None, 3_500, 0):  # k936hc8w
+            config.lr = 9e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 5_000
+            config.warmup_steps_pct = 0.2
+        case ("VAE", "galaxy_mnist", None, 3_500, 1):  # a32x750r
+            config.lr = 9e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 5_000
+            config.warmup_steps_pct = 0.2
+        case ("VAE", "galaxy_mnist", None, 3_500, 2):  # f56hqg6r
+            config.lr = 3e-3
+            config.model.conv_dims = (64, 128, 256)
+            config.steps = 5_000
+            config.warmup_steps_pct = 0.2
 
     config.run_iwlb = False
     config.iwlb_num_samples = 300
 
     config.batch_size = 512
-    config.batch_size_eval = 50
-    config.num_val = 10000
-    add_mnist_config(
-        config,
-        angle=config.angle,
-        num_trn=config.get("num_trn", None),
-        num_val=config.num_val,
-    )
+    if config.dataset == "MNIST":
+        config.batch_size_eval = 50
+        config.num_val = 10000
+        add_mnist_config(
+            config,
+            angle=config.angle,
+            num_trn=config.get("num_trn", None),
+            num_val=config.num_val,
+        )
+    elif config.dataset == "galaxy_mnist":
+        config.batch_size_eval = 16
+        config.num_val = 1000
+        add_galaxy_mnist_config(
+            config,
+            num_trn=config.get("num_trn", None),
+            num_val=config.num_val,
+        )
     config.test_split = ""
 
     return config
--- experiments/sweeps/create_vae_augvae_best_sweeps.py
+++ experiments/sweeps/create_vae_augvae_best_sweeps.py
@@ -6,27 +6,43 @@ from experiments.utils import format_thousand
 ENTITY = "invariance-learners"
 PROJECT = "icml2024"
 CHECKPOINT_DIR = "/home/jua23/rds/hpc-work/learning-invariances-models"
+MAX_NUM_RUNS = 36
 ANGLES = [
-    0,
-    15,
-    90,
-    180,
+    # 0,
+    # 15,
+    # 90,
+    # 180,
+    None
 ]
 NUM_TRNS = [
-    50_000,
-    37_500,
-    25_000,
+    # 50_000,
+    # 37_500,
+    # 25_000,
+    # 12_500,
+    7_000,
+    3_500,
 ]
 MODEL_NAMES = [
-    # "augvae",
-    "invvae",
-    # "vae",
+    "augvae",
+    # "invvae",
+    "vae",
+    # "vae_wsda",
 ]
 SEEDS = [
     0,
-    # 1,
-    # 2,
+    1,
+    2,
 ]
+DATASETS = [
+    # "MNIST",
+    "galaxy_mnist",
+]
+
+fmt_model_name = {
+    "vae": "VAE",
+    "augvae": "AugVAE",
+    "invvae": "InvVAE",
+}
 
 parent_path = Path(__file__).parent
 
@@ -34,28 +50,28 @@ for model_name in MODEL_NAMES:
     job_folder = parent_path.parent / "jobs" / f"{model_name}_best_sweep"
     job_folder.mkdir(exist_ok=True)
 
-    job_file = job_folder / f"{model_name}_best_sweep%01:00:00.txt"
+    job_file = job_folder / f"{model_name}_best_sweep%01:30:00.txt"
     if job_file.exists():
         job_file.unlink()
 
     with job_file.open("w") as jf:
-        for angle, num_trn, seed in product(ANGLES, NUM_TRNS, SEEDS):
-            # match (model_name, angle, num_trn, seed):
-            #     case ("vae" | "augvae", 0 | 180, 50_000 | 25_000, 0):
-            #         continue
+        for dataset, angle, num_trn, seed in product(DATASETS, ANGLES, NUM_TRNS, SEEDS):
+            if dataset == "MNIST" and (num_trn is None or angle is None):
+                continue
 
-            model_name_lookup = {
-                "vae": "VAE",
-                "augvae": "AugVAE",
-                "invvae": "InvVAE",
-            }
+            if (dataset == "galaxy_mnist") and (num_trn is None or angle is not None):
+                continue
 
-            params = ("MNIST", seed, angle, num_trn)
+            match dataset:
+                case "MNIST":
+                    params = (dataset, seed, angle, num_trn)
+                case "galaxy_mnist":
+                    params = (dataset, seed, num_trn)
             params = [str(p) for p in params]
+
             if model_name in ["augvae", "invvae"]:
                 extra_args = (
-                    f"--vae_config=experiments/configs/vae_best.py:{model_name_lookup[model_name]},{angle},{num_trn} "
-                    f"--vae_config.seed={seed} "
+                    f"--vae_config=experiments/configs/vae_best.py:{fmt_model_name[model_name]},{','.join(params)} "
                     "--vae_config.test_split=test "
                     f"--gen_config=experiments/configs/gen_best.py:{','.join(params)} "
                     f"--gen_config.checkpoint={CHECKPOINT_DIR}/gen_best_ckpt_{'_'.join(params)} "
@@ -64,8 +80,7 @@ for model_name in MODEL_NAMES:
                 )
             else:
                 extra_args = (
-                    f"--config=experiments/configs/vae_best.py:vae,{angle},{num_trn} "
-                    f"--config.seed={seed} "
+                    f"--config=experiments/configs/vae_best.py:{fmt_model_name[model_name]},{','.join(params)} "
                     "--config.test_split=test "
                 )
 
